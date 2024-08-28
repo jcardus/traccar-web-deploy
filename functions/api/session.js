e0f07fc2e6c837f6bb2daaa71900a1d9b043fb97
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-28 08:24:01 +0100

    invalidate session on non get

--- functions/api/session.js
+++ functions/api/session.js
@@ -4,7 +4,7 @@ export async function onRequest({request, env}) {
     const url = new URL(request.url);
     const pattern = new URLPattern({ pathname: '/api/session/:id' });
     const match = pattern.exec(url);
-    if (match && request.method === 'GET') {
+    if (request.method !== 'GET' || match) {
         await invalidateSession(getJSessionId(request));
     }
     return forwardWithCache({request, env});
