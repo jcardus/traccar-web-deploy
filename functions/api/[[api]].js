767882f206cd64400bfa09d3aa000e0455e5c05c
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-28 03:26:01 +0100

    catch all errors

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,42 +1,47 @@
 export async function onRequest({request, env}) {
-    const url = new URL(request.url)
-    const cookie = request.headers.get('Cookie') || '';
+    try {
+        const url = new URL(request.url)
+        const cookie = request.headers.get('Cookie') || '';
 
-    if (url.pathname === '/api/positions' && env.POSITIONS_SERVER) {
-        const jSessionId = getCookie(cookie, 'JSESSIONID')
-        if (jSessionId) {
-            url.searchParams.set('JSESSIONID', jSessionId)
+        if (url.pathname === '/api/positions' && env.POSITIONS_SERVER) {
+            const jSessionId = getCookie(cookie, 'JSESSIONID')
+            if (jSessionId) {
+                url.searchParams.set('JSESSIONID', jSessionId)
+            }
+            url.hostname = env.POSITIONS_SERVER
+            return Response.redirect(url, 302)
         }
-        url.hostname = env.POSITIONS_SERVER
-        return Response.redirect(url, 302)
+
+        const auth = request.headers.get('Authorization') || '';
+        const cacheKey = new Request(
+            `${url.toString()}${url.search ? '&' : '?'}cookie=${encodeURIComponent(cookie)}&auth=${encodeURIComponent(auth)}`,
+            {method: request.method, headers: request.headers}
+        );
+        const cache = caches.default;
+        let response = await cache.match(cacheKey);
+
+        if (!response || request.method !== 'GET') {
+            console.log(`${cacheKey} not present in cache. Fetching and caching request.`,);
+            url.host = env.TRACCAR_SERVER
+            url.protocol = 'http:'
+            response = await fetch(new Request(url, request))
+            if (!response.ok) {
+                console.error(response.status, await response.text())
+                return new Response('Error ' + response.status, {status: response.status});
+            }
+            if (request.method !== 'GET') {
+                response = new Response(response.body, response);
+                response.headers.append("Cache-Control", "s-maxage=10");
+                ctx.waitUntil(cache.put(cacheKey, response.clone()));
+            }
+        } else {
+            console.log(`Cache hit for: ${cacheKey}.`);
+        }
+        return response
+    } catch (e) {
+        console.error(e)
+        return new Response(e.message, {status: 500});
     }
-
-    const auth = request.headers.get('Authorization') || '';
-    const cacheKey = new Request(
-        `${url.toString()}${url.search ? '&' : '?'}cookie=${encodeURIComponent(cookie)}&auth=${encodeURIComponent(auth)}`,
-        { method: request.method, headers: request.headers }
-    );
-    const cache = caches.default;
-    let response = await cache.match(cacheKey);
-
-    if (!response || request.method !== 'GET') {
-        console.log(`${cacheKey} not present in cache. Fetching and caching request.`,);
-        url.host = env.TRACCAR_SERVER
-        url.protocol = 'http:'
-        response = await fetch(new Request(url, request))
-        if (!response.ok) {
-            console.error(response.status, await response.text())
-            return new Response('server error ' + response.status, {status: response.status});
-        }
-        if (request.method !== 'GET') {
-            response = new Response(response.body, response);
-            response.headers.append("Cache-Control", "s-maxage=10");
-            ctx.waitUntil(cache.put(cacheKey, response.clone()));
-        }
-    } else {
-        console.log(`Cache hit for: ${cacheKey}.`);
-    }
-    return response
 }
 
 function getCookie(cookies, name) {
