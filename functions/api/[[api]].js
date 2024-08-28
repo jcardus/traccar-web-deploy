0fea8e7fb16a581604170dee0144c83361079729
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-28 09:20:10 +0100

    url is not defined

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,8 +1,8 @@
 import {forwardWithCache, getBasePath, invalidatePath} from "../utils";
-export async function onRequest({request, env}) {
+export async function onRequest({request, env, functionPath}) {
     if (request.method !== 'GET') {
-        console.log(`Invalidate path due to ${request.method} ${url.pathname}`);
-        await invalidatePath(getBasePath(new URL(request.url).pathname), env);
+        console.log(`Invalidate path due to ${request.method} ${functionPath}`);
+        await invalidatePath(getBasePath(functionPath), env);
     }
     return forwardWithCache({request, env});
 }
--- functions/api/positions.js
+++ functions/api/positions.js
@@ -2,6 +2,7 @@ import {forward, getCookie} from "../utils";
 
 export async function onRequest({request, env}) {
     if (env.POSITIONS_SERVER) {
+        const url = new URL(request.url);
         const jSessionId = getCookie(request.headers.get('Cookie'), 'JSESSIONID')
         if (jSessionId) { url.searchParams.set('JSESSIONID', jSessionId) }
         url.hostname = env.POSITIONS_SERVER
