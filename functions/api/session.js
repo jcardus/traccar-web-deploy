6d8528c0cb22100ab7dc662de5aff31b2eba8bf8
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-28 08:19:09 +0100

    exec with full url

--- functions/api/session.js
+++ functions/api/session.js
@@ -3,7 +3,7 @@ import {forwardWithCache, getJSessionId, invalidateSession} from "../utils";
 export async function onRequest({request, env}) {
     const url = new URL(request.url);
     const pattern = new URLPattern({ pathname: '/api/session/:id' });
-    const match = pattern.exec(url.pathname);
+    const match = pattern.exec(url);
     if (match && request.method === 'GET') {
         await invalidateSession(getJSessionId(request));
     }
