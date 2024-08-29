1c0831d8d01e9a2417c452d50e2e33e2dafcd55e
jcardus <asklocation.net@gmail.com>
2024-08-29 05:48:11 +0100

    clear cache on permissions.js

--- functions/api/permissions.js
+++ functions/api/permissions.js
@@ -1,6 +1,9 @@
-import {invalidateCache, getJSessionId, forward} from "../utils";
+import {invalidateCache, forward} from "../utils";
 
 export async function onRequest({request, env}) {
-    await invalidateCache(env, getJSessionId(request));
+    const json = await request.clone().json()
+    for (const key of Object.keys(json).filter(k => k !== 'deviceId')) {
+        await invalidateCache(env, '', `${new URL(request.url).origin}/api/${key.replace('Id', '')}`);
+    }
     return forward({request, env});
 }
