957ae6c8525955f0a49edf08ecbd5879505c07ca
jcardus <asklocation.net@gmail.com>
2024-08-29 04:55:36 +0100

    forward permissions.js

--- functions/api/permissions.js
+++ functions/api/permissions.js
@@ -1,8 +1,2 @@
-import {forwardWithCache} from "../utils";
-export async function onRequest({request, env, functionPath}) {
-    if (request.method !== 'GET') {
-        console.log(`Invalidate all due to ${request.method} ${functionPath}`);
-        await invalidateAll();
-    }
-    return forwardWithCache({request, env});
-}
+import {forward} from "../utils";
+export const onRequest = forward
