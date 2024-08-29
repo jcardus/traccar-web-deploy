6ea4c10dca75cdefe9a5dc40d4e77b4d090defbd
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-29 06:57:33 +0100

    invalidate session

--- functions/api/session.js
+++ functions/api/session.js
@@ -1,2 +1,8 @@
-import {forward} from "../utils";
-export const onRequest = forward
+import {forward, getJSessionId, invalidateCache} from "../utils";
+export async function onRequest({request, env, functionPath}) {
+    if (request.method !== 'GET') {
+        console.log(`session invalidate due to ${request.method} ${functionPath}`);
+        await invalidateCache(env, getJSessionId(request));
+    }
+    return forward({request, env})
+}
