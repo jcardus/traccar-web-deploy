831acba0535d48b3b5503aa0c8f2c608cf504aae
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-29 01:59:27 +0100

    sentry 3

--- src/sentry.js
+++ src/sentry.js
@@ -1,9 +1,5 @@
 import * as Sentry from "@sentry/react";
 Sentry.init({
-    dsn: "https://bad6baf4fb2072316e0edf5a1a15e994@o4507747429974016.ingest.us.sentry.io/4507857547231232",
-    integrations: [
-        Sentry.browserTracingIntegration()
-    ],
-    tracesSampleRate: 1.0,
-    tracePropagationTargets: ["localhost", /^https:\/\/web.rastreosat.com.br\/api/]
+    dsn: "https://bad6baf4fb2072316e0edf5a1a15e994@o4507747429974016.ingest.us.sentry.io/4507857547231232"
 });
+
