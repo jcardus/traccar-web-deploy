71745eb4f4b75ca4563b2ae8de3fc19732653ccb
jcardus <asklocation.net@gmail.com>
2024-08-29 04:36:53 +0100

    pass undefined catch exception

--- functions/api/devices/[[device]].js
+++ functions/api/devices/[[device]].js
@@ -1,10 +1,15 @@
 import {forward} from '../../utils'
 export async function onRequest({request, env, functionPath}) {
-    console.log('devices', functionPath)
-    return forward({request, env}, request.method === 'GET' && {
-        cf: {
-            cacheTtl: 10,
-            cacheEverything: true,
-        }
-    })
+    try {
+        console.log('devices', functionPath)
+        return forward({request, env}, request.method === 'GET' ? {
+            cf: {
+                cacheTtl: 10,
+                cacheEverything: true,
+            }
+        } : undefined)
+    } catch (e) {
+        console.error(e)
+        return new Response(e.message, {status: 500})
+    }
 }
