d643670de0c06bc15aebff61fbc30fdcf9052f3b
entrack-plataforma <entrack.plataforma@gmail.com>
2024-08-29 02:29:25 +0100

    don't cache non get requests

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,8 +1,9 @@
-import {forwardWithCache, getBaseUrl, invalidateCache} from "../utils";
+import {forward, forwardWithCache, getBaseUrl, invalidateCache} from "../utils";
 export async function onRequest({request, env, functionPath}) {
     if (request.method !== 'GET') {
         console.log(`Invalidate path due to ${request.method} ${functionPath}`);
         await invalidateCache(env, '', getBaseUrl(request.url));
+        return forward({request, env})
     }
     return forwardWithCache({request, env});
 }
