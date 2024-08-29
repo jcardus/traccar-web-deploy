51ed87756a83a5d7577612ce9644e853282890bb
jcardus <asklocation.net@gmail.com>
2024-08-29 04:25:32 +0100

    fetch with cache

--- functions/api/devices/[[device]].js
+++ functions/api/devices/[[device]].js
@@ -1,2 +1,10 @@
 import {forward} from '../../utils'
-export const onRequest = forward
+export async function onRequest({request, env, functionPath}) {
+    console.log('devices', functionPath)
+    return forward({request, env}, request.method === 'GET' && {
+        cf: {
+            cacheTtl: 10,
+            cacheEverything: true,
+        }
+    })
+}
