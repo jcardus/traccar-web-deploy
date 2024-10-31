4c5f245cebf87ba344a7fb13251576838f8fd48c
jcardus <joaquim_cardeira@hotmail.com>
2024-10-31 12:58:20 +0000

    remove cache

---
+++ functions/api-cache/[[api]].js
@@ -0,0 +1,17 @@
+import {forward, forwardWithCache, getBaseUrl, getJSessionId, invalidateCache} from "../utils";
+export async function onRequest({request, params, env, functionPath}) {
+    if (!env.CACHE_KEYS) { return forward({request, env}) }
+    const jSessionId = getJSessionId(request);
+    if (request.method !== 'GET') {
+        console.log(`Invalidate path due to ${request.method} ${functionPath}`);
+        await env.CACHE_KEYS.put(jSessionId, new Date().getTime())
+        await invalidateCache(env, getBaseUrl(request.url, params.api[0]));
+        return forward({request, env})
+    }
+    const lastInvalid = await env.CACHE_KEYS.get(jSessionId)
+    return forwardWithCache(
+        {request, env},
+        jSessionId,
+        new Date().getTime() - lastInvalid < 1000 * 60
+    );
+}
